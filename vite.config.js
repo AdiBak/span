31e632da26629ab5a157202a0e5564ef43aa58b9
Aditya Bakshi <50807887+AdiBak@users.noreply.github.com>
2025-11-15 14:17:42 -0800

    Fix: Add publicDir for assets and GitHub Secrets for env vars

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
+  publicDir: 'assets',
   plugins: [
     react(),
     // Plugin to inject environment variables for vanilla JS modules
